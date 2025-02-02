fe4e0ef9bd67a60862cbc717bf0c2b8aade3d03b
Ayinla <sherifdeenraji96@gmail.com>
2025-02-02 22:01:25 +0100

    modify the helper to improve usage

---
+++ helpers/helpers.js
@@ -0,0 +1,29 @@
+const isPerfect = (num) => {
+    if (num <= 1) return false;
+    let sum = 0;
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) sum += i;
+    }
+    return sum === num;
+}
+
+const isPrime = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+const isArmstrong = (num) => {
+    const digits = num.toString().split('');
+    const numDigits = digits.length;
+
+    const sum = digits.reduce((acc, digit) => {
+        return acc + Math.pow(Number(digit), numDigits);
+    }, 0);
+
+    return sum === num;
+}
+
+
+module.exports = { isPerfect, isPrime, isArmstrong };
--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const express = require('express');
 require('dotenv').config();
-const isPrime = require('./helpers/isPrime');
-const isPerfect = require('./helpers/isPerfect');
+const { isPerfect, isPrime, isArmstrong } = require('./helpers/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.get('/api/classify-number', (req, res) => {
+    try {
+
+        const query = req.query;
+        if (Object.keys(query).length !== 1) {
+            return res.status(400).json({ message: 'Please provide only one parameter (positive integer) for you query' });
+        }
+        if (query.number === undefined) {
+            return res.status(400).json({ message: 'Please provide a number as a query for your api' });
+        }
+        if (query.number < 0) {
+            return res.status(400).json({ message: 'Please provide a positive number' });
+        }
+        const number = query.number;
+        const is_prime = isPrime(number);
+        const is_perfect = isPerfect(number);
+        const properties = [];
+        if (isArmstrong(number)) {
+            properties.push('armstrong');
+        }
+        if (number % 2 !== 0) {
+            properties.push('odd');
+        }
+        if (number % 2 === 0) {
+            properties.push('even');
+        }
+        const digit_sum = num.toString().split('').map(Number).reduce((acc, curr) => acc + curr, 0);
+
+        res.status(200).json({ number, is_prime, is_perfect, properties, digit_sum });
+    } catch (error) {
+        res.status(`500`).json({ message: 'Internal server error' });
+    }
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+const obj = { fi: 'sh', se: 'co', th: 'de' };
+
 
 //api/classify-number?number=371
 
